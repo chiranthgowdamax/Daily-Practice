91c7f0f340d0ffaf2a4d77917dc1099a2155b67d
Chiranth <chiranthgowda345@gmail.com>
2026-01-28 11:37:06 +0530

    Fix folder structure: movie_tickets_bill inside Day-02

--- movie_tickets_bill/main.py
+++
@@ -1,8 +0,0 @@
-movie_name = input("Enter movie name: ")
-ticket_price = float(input("Enter ticket price: "))
-no_of_tickets = int(input("Enter the number of tickets: "))
-
-total_cost = ticket_price * no_of_tickets
-
-print(f"Movie: {movie_name}")
-print(f"Total cost is ₹{total_cost:.2f}")
